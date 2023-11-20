3bb49ff63295910fa5f8f3047fabd8789a6f8916
Sumanth <satyasaisumanth7@gmail.com>
2023-11-20 12:28:53 +0530

    added a new file

---
+++ Evenodd.java
@@ -0,0 +1,20 @@
+package pro;
+import java.util.Scanner;
+public class Evenodd {
+
+	public static void main(Str[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter number");
+		int n=sc.nextInt();
+		if(n%2==0)
+			System.out.println(n+" is even number");
+			else
+				System.out.println(n+" is a odd number");
+		
+		
+		
+		
+
+	}
+
+}
